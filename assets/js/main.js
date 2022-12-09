9c7cd37898eeda668c34e4fef4cfdb96f06f9ef0
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-12-09 18:29:11 +0800

    feat: swiper popular card

--- assets/js/main.js
+++ assets/js/main.js
@@ -10,6 +10,17 @@ const scrollHeader = () => {
 window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
+const popularSwiper = new Swiper(".popular__container", {
+    spaceBetween: 32,
+    grabCursor: true,
+    centeredSlides: true,
+    slidesPerView: "auto",
+    loop: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
 
 /*=============== VALUE ACCORDION ===============*/
 
