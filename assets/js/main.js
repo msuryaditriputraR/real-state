10e6f82abe3b35b011466ba1a8d2bde5c3a6581d
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-12-09 17:53:27 +0800

    change background header

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,22 @@
 /*=============== CHANGE BACKGROUND HEADER ===============*/
+const scrollHeader = () => {
+    const header = document.getElementById("header");
 
+    this.scrollY >= 80
+        ? header.classList.add("scroll-header")
+        : header.classList.remove("scroll-header");
+};
+
+window.addEventListener("scroll", scrollHeader);
 
 /*=============== SWIPER POPULAR ===============*/
 
-
 /*=============== VALUE ACCORDION ===============*/
-
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
 
+/*=============== SHOW SCROLL UP ===============*/
 
-/*=============== SHOW SCROLL UP ===============*/ 
-
-
-/*=============== DARK LIGHT THEME ===============*/ 
-
+/*=============== DARK LIGHT THEME ===============*/
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
