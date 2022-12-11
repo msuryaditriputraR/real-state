3d9b3fff7cf87c5338cf4625fbc1218dfc0c189e
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2022-12-11 09:20:32 +0800

    active link when scrolling :

--- assets/js/main.js
+++ assets/js/main.js
@@ -34,6 +34,28 @@ accordionItems.forEach((item) => {
 });
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll("section[id]");
+
+function scrollActive() {
+    const scrollY = window.scrollY;
+
+    sections.forEach((current) => {
+        const sectionHeight = current.offsetHeight;
+        const sectionTop = current.offsetTop - 50;
+        const sectionId = current.getAttribute("id");
+        const sectionLink = document.querySelector(
+            `.nav__menu a[href*=${sectionId}]`
+        );
+
+        if (scrollY > sectionTop && scrollY <= sectionTop + sectionHeight) {
+            sectionLink.classList.add("active-link");
+        } else {
+            sectionLink.classList.remove("active-link");
+        }
+    });
+}
+
+window.addEventListener("scroll", scrollActive);
 
 /*=============== SHOW SCROLL UP ===============*/
 
